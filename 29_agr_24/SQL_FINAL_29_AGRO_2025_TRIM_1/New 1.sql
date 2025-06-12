d8cd89ee7144d7c73a093713ba2840c4df8f2954
Vitalie Bancu <64257883+androidacvtivity@users.noreply.github.com>
2025-06-12 13:08:16 +0300

    45

---
+++ 29_agr_24/SQL_FINAL_29_AGRO_2025_TRIM_1/New 1.sql
@@ -0,0 +1,165 @@
+--
+--CREATE OR REPLACE FORCE VIEW VW_36_CNT
+--(
+--
+--
+--    CUIIO,
+--    DENUMIRE,
+--    CUATM,
+--    COL1 
+-- 
+-- )
+--AS 
+
+SELECT 
+    CUIIO,
+    DENUMIRE,
+    CUATM,
+    COL1 
+FROM
+(
+SELECT 
+    L.CUIIO,
+    C.DENUMIRE,
+    L.CUATM,
+    C.FULL_CODE,
+    L.COL1    
+            FROM  (
+             SELECT
+    DISTINCT 
+    D.CUIIO,
+     D.CUIIO_VERS,
+     D.CUATM,
+    D.FORM,
+    D.DATA_REG,
+    D.COL1
+    
+    FROM 
+
+    (
+    SELECT DISTINCT     D.CUIIO,
+                         D.CUIIO_VERS,
+                          D.CUATM,
+                          D.FORM,
+                          MAX(D.DATA_REG) DATA_REG,
+                          'CIS2' AS COL1
+                          
+            FROM CIS2.VW_DATA_ALL D
+            
+                 
+           WHERE D.PERIOADA = :pPERIOADA AND D.FORM IN (:pFORM)
+           
+           
+          
+           
+           AND D.CUIIO NOT IN (
+            SELECT DISTINCT D.CUIIO
+                          
+                          
+            FROM USER_EREPORTING.VW_DATA_ALL_PRIMIT D
+                                        INNER JOIN CIS2.RENIM R ON R.CUIIO = D.CUIIO AND R.CUIIO_VERS = D.CUIIO_VERS 
+                
+           WHERE D.PERIOADA  = :pPERIOADA   AND D.FORM IN (:pFORM)
+             AND D.ID_SCHEMA = '2'
+           
+           )
+          
+
+            GROUP BY
+             D.CUIIO,
+                         D.CUIIO_VERS,
+                          D.CUATM,
+                          D.FORM
+           
+           UNION 
+           
+            SELECT DISTINCT D.CUIIO,
+                          D.CUIIO_VERS,
+                          D.CUATM,
+                          D.FORM,
+                          MAX(D.DATA_REG) DATA_REG,
+                          'EREPORTING' AS COL1
+            FROM USER_EREPORTING.VW_DATA_ALL_PRIMIT D
+                INNER JOIN CIS2.RENIM R ON R.CUIIO = D.CUIIO AND R.CUIIO_VERS = D.CUIIO_VERS 
+           WHERE D.PERIOADA  = :pPERIOADA   AND D.FORM IN (:pFORM)
+             AND D.ID_SCHEMA = '2'
+             
+            GROUP BY
+             D.CUIIO,
+                         D.CUIIO_VERS,
+                          D.CUATM,
+                          D.FORM
+            
+             
+             
+             ) D  LEFT JOIN (
+             
+              SELECT DISTINCT 
+                          D.CUIIO,
+                          D.CUIIO_VERS,
+                          D.CUATM,
+                          D.FORM,
+                          MAX(D.DATA_REG) DATA_REG,
+                          'CIS2' AS COL1
+                          
+            FROM CIS2.VW_DATA_ALL D
+            
+                 
+           WHERE D.PERIOADA = :pPERIOADA AND D.FORM IN (:pFORM)
+           
+           GROUP BY 
+            D.CUIIO,
+                          D.CUIIO_VERS,
+                          D.CUATM,
+                          D.FORM
+                         
+           
+             ) R ON D.CUIIO = R.CUIIO
+             
+             
+             WHERE
+             
+             R.CUIIO IS NOT NULL
+            ) L 
+            
+                   INNER JOIN CIS2.VW_CL_CUATM C ON C.CODUL = L.CUATM
+                   
+                   UNION ALL 
+                   
+                   
+                   
+    SELECT 
+    NULL CUIIO,
+    DENUMIRE,
+    CODUL CUATM,
+    FULL_CODE,
+    NULL COL1 
+                   
+                   FROM CIS2.VW_CL_CUATM
+                   
+                   WHERE 
+                   PRGS IN ('2')
+                   AND CODUL NOT IN ('0000000')
+                   
+                   
+                   
+                   ORDER BY 
+                   FULL_CODE )
+                   
+                   
+                   WHERE 
+                   1=1 
+              --     AND CUATM LIKE '01%'
+                   
+                   
+                   
+                   
+                   ;
+                   
+                   
+                   
+--SELECT *
+--   
+--
+--      FROM  VW_36_CNT;
+                   
